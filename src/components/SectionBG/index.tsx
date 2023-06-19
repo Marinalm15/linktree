4df7b4c89ab388c874ff1eada9b8c66b893731f3
Marina Mendonça <marinalm15@hotmail.com>
2023-06-19 13:39:15 -0300

    adição de links de artigos dinamicamente

---
+++ src/components/SectionBG/index.tsx
@@ -0,0 +1,19 @@
+import { links } from "../../helpers/Links";
+
+export const SectionBG = () => {
+  return (
+    <div className="section_BG">
+      <div className="section_container">
+        {links.map((links) => {
+          return (
+            <div className="links_content">
+              <a key={links.id} href="#">
+                <p className="section_container_text">{links.name}</p>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
---
+++ src/helpers/Links.ts
@@ -0,0 +1,16 @@
+export const links = [
+  {
+    id: 1,
+    name: "Artigo do Blog: A importância de uma boa vistoria",
+  },
+
+  {
+    id: 2,
+    name: "Blog da InMediam - Garantia Locatícia",
+  },
+
+  {
+    id: 3,
+    name: "Site da InMediam - Garantia Locatícia",
+  },
+];
