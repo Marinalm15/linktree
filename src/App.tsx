45bf6398cd1d11ea10d2e382858dc435ad4bcd66
Marina Mendonça <marinalm15@hotmail.com>
2023-06-21 16:30:13 -0300

    foi retirado um ';' que estava a mais

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export function App() {
   return (
     <ClerkProvider publishableKey={publishableKey}>
       <BrowserRouter>
-        <Router />;
+        <Router />
       </BrowserRouter>
     </ClerkProvider>
   );
