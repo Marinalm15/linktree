ab34543859b8b65744c19b781764b515a6d33382
Marina Mendonça <marinalm15@hotmail.com>
2023-06-22 16:48:38 -0300

    introdução do clerk themes para personalização da página de log in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router.tsx";
-import { ClerkProvider, UserButton } from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ptBR } from "@clerk/localizations";
+import "./style.css";
 
 const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 export function App() {
   return (
-    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkProvider
+      publishableKey={publishableKey}
+      localization={ptBR}
+      appearance={{
+        elements: {
+          formButtonPrimary: "button_login",
+          logoImage: "logo_img",
+          logoBox: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100px",
+          },
+        },
+        variables: {
+          colorBackground: "#cbdf24",
+        },
+        layout: {
+          socialButtonsPlacement: "bottom",
+          socialButtonsVariant: "iconButton",
+        },
+      }}
+    >
       <BrowserRouter>
         <Router />
       </BrowserRouter>
