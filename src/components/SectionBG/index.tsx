8f7c23be5ab5ad440c2c3b0c856dbb89cf5ad5e0
Marina Mendonça <marinalm15@hotmail.com>
2023-06-29 13:20:17 -0300

    colocação de scroll na página inicial de maneira a aparecerm mais de três artigos

--- src/components/SectionBG/index.tsx
+++ src/components/SectionBG/index.tsx
@@ -4,15 +4,17 @@ export const SectionBG = () => {
   return (
     <div className="section_BG">
       <div className="section_container">
-        {links.map((links) => {
-          return (
-            <div key={links.id} className="links_content">
-              <a href={links.url}>
-                <p className="section_container_text">{links.name}</p>
+        <div className="section_article_wrapper">
+          {links.map((links) => {
+            return (
+              <a key={links.id} href={links.url}>
+                <div className="links_content">
+                  <p className="section_container_text">{links.name}</p>
+                </div>
               </a>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
