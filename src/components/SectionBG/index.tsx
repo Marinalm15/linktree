a6f67605d82851495e9bc1ece694916c21c8913e
Marina Mendonça <marinalm15@hotmail.com>
2023-07-04 16:43:20 -0300

    chamada da api na imagem principal

--- src/components/SectionBG/index.tsx
+++ src/components/SectionBG/index.tsx
@@ -1,19 +1,43 @@
-import { links } from "../../helpers/Links";
+import { useEffect } from "react";
+import { useArticle } from "../../hooks/useArticle";
 
 export const SectionBG = () => {
+  const { listArticles, articles, setArticles } = useArticle();
+
+  useEffect(() => {
+    let ignore = false;
+    setArticles([]);
+
+    listArticles()
+      .then((response) => {
+        if (!ignore) {
+          setArticles(response.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <div className="section_BG">
       <div className="section_container">
         <div className="section_article_wrapper">
-          {links.map((links) => {
-            return (
-              <a key={links.id} href={links.url}>
-                <div className="links_content">
-                  <p className="section_container_text">{links.name}</p>
-                </div>
-              </a>
-            );
-          })}
+          {articles &&
+            articles.map((links) => {
+              return (
+                <a key={links._id} href={links.url}>
+                  <div className="links_content">
+                    {/* <img className="img_link" src={links.imagem} /> */}
+                    <p className="section_container_text">{links.name}</p>
+                  </div>
+                </a>
+              );
+            })}
         </div>
       </div>
     </div>
